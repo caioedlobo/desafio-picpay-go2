6b2a8452c1cd523d926ee75318a086951caaaf55
caioedlobo <caioeduardolobo@gmail.com>
2025-09-30 18:54:31 -0300

    feat: create readJson() on handlers.go

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/labstack/echo/v4"
 )
@@ -17,4 +18,11 @@ func NewHandler() *Handler {
 func (h Handler) Listen(port string) error {
 	h.registerUserEndpoints()
 	return h.echo.Start(fmt.Sprintf(":%s", port))
+}
+
+func readJSON(c echo.Context, obj any) error {
+	reqBody := c.Request().Body
+	dec := json.NewDecoder(reqBody)
+	dec.DisallowUnknownFields()
+	return dec.Decode(obj)
 }
