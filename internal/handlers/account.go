535c4f8c30b7c5d3dc6191bacc6d532382ed1384
caioedlobo <caioeduardolobo@gmail.com>
2025-09-30 18:54:55 -0300

    feat: create createUser handler on handlers/account.go

--- internal/handlers/account.go
+++ internal/handlers/account.go
@@ -1,14 +1,21 @@
 package handlers
 
 import (
+	"desafio-picpay-go2/internal/models/user/dto"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 func (h Handler) registerUserEndpoints() {
-	h.echo.GET("/users", h.helloWorld)
+	h.echo.POST("/users", h.createUser)
 }
 
-func (h Handler) helloWorld(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+func (h Handler) createUser(c echo.Context) error {
+	var createUserDTO dto.CreateUserRequest
+	err := readJSON(c, &createUserDTO)
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusCreated, createUserDTO)
 }
