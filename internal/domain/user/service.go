c4aabe0e4c74f328e0a3464af7fdfc2f90ff3097
caio <caioeduardolobo@gmail.com>
2025-10-07 19:48:10 -0300

    feat: create user service

---
+++ internal/domain/user/service.go
@@ -0,0 +1,65 @@
+package user
+
+import (
+	"context"
+	"desafio-picpay-go2/internal/common/dto"
+	"desafio-picpay-go2/internal/domain/user/value_object"
+	"errors"
+)
+
+var ErrEmailAlreadyExists = errors.New("email already registered")
+
+type Service struct {
+	repo UserRepository
+}
+
+func NewService(repo UserRepository) *Service {
+	return &Service{repo: repo}
+}
+
+type UserRepository interface {
+	Save(context.Context, *User) error
+	FindByEmail(context.Context, string) (*User, error)
+}
+
+func (s Service) Save(ctx context.Context, input dto.CreateUserRequest) (*dto.CreateUserResponse, error) {
+	if userExists, _ := s.repo.FindByEmail(ctx, input.Email); userExists != nil {
+		return nil, ErrEmailAlreadyExists
+	}
+
+	name, err := value_object.NewName(input.Name)
+	if err != nil {
+		return nil, err
+	}
+	docNumber, err := value_object.NewDocumentNumber(input.DocumentNumber)
+	if err != nil {
+		return nil, err
+	}
+	docType, err := value_object.NewDocumentType(input.DocumentType)
+	if err != nil {
+		return nil, err
+	}
+	email, err := value_object.NewEmail(input.Email)
+	if err != nil {
+		return nil, err
+	}
+	password, err := value_object.NewPassword(input.Password)
+	if err != nil {
+		return nil, err
+	}
+	u, err := NewUser(name, docNumber, docType, email, *password)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = s.repo.Save(ctx, u); err != nil {
+		return nil, err
+	}
+	return &dto.CreateUserResponse{
+		Id:             u.ID.String(),
+		Name:           u.Name.String(),
+		DocumentNumber: u.DocumentNumber.String(),
+		DocumentType:   u.DocumentType.String(),
+		Email:          u.Email.String(),
+	}, nil
+}
