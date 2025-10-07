eeab5de8963b7520721743085163cfa28b9b2067
caio <caioeduardolobo@gmail.com>
2025-10-07 19:48:21 -0300

    refac: remove old dto package

--- internal/dto/user.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type CreateUserRequest struct {
-	Name           string `json:"name" validate:"required,lte=100"`
-	DocumentNumber string `json:"document_number" validate:"required,gte=11,lte=14"`
-	DocumentType   string `json:"document_type" validate:"required,gte=3,lte=4"`
-	Email          string `json:"email" validate:"required,email"`
-	Password       string `json:"password" validate:"required,lte=100"`
-}
