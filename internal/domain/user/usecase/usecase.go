3f9c9aa4d658712d2ac16c9a4bb7d6050d69f3d7
caio <caioeduardolobo@gmail.com>
2025-10-07 19:46:54 -0300

    refac: remove usecases and repository unused code

--- internal/domain/user/usecase/usecase.go
+++
@@ -1,9 +0,0 @@
-package usecase
-
-type UserUseCase struct {
-	CreateUserRepo UserRepository
-}
-
-func NewUserCase(createUserRepo UserRepository) *UserUseCase {
-	return &UserUseCase{CreateUserRepo: createUserRepo}
-}
